bba2d7d17fd1e94b7463c6a81be3524766b3daf6
albgarci <albgarci@student.42madrid.com>
2021-12-22 17:59:39 +0100

    rule of no repeating lowercase ok, but now only shows 1 path...

--- day-12/paths.c
+++ day-12/paths.c
@@ -20,8 +20,14 @@ int		count_nodes(t_edge **edges);
 void	add_node(t_node **nodes, char node_name[10]);
 t_node **separate_nodes(t_edge **edges);
 char	*get_node_name(t_node **nodes, int pos);
-void	create_matrix(t_node **nodes, t_edge **edges);
+int		**create_matrix(t_node **nodes, t_edge **edges, int	n_nodes);
 int		get_node_position(t_node **nodes, char *name);
+int		get_num_nodes(t_node **nodes);
+void	find_paths(int **matrix, t_node **nodes, int num_nodes);
+void	continue_path(int **matrix, t_node **nodes, t_node **path, int num_nodes, int next_step, int *j);
+void	add_to_path(t_node **path, char *name);
+int		check_path(t_node **path, int current, char	*next);
+void	print_path(t_node **path);
 
 int	main(void)
 {
@@ -30,7 +36,10 @@ int	main(void)
 	char	line[20] = {0};
 	t_edge	*aux;
 	t_node	**nodes;
+	int		**matrix;
+	int		num_nodes;
 
+	matrix = 0;
 	nodes = 0;
 	f = fopen("ex", "r");
 	edges = malloc(sizeof(t_edge *));
@@ -44,15 +53,149 @@ int	main(void)
 	printf("\n---------\n\n");
 	aux = *edges;
 	nodes = separate_nodes(edges);
-	create_matrix(nodes, edges);
+	num_nodes = get_num_nodes(nodes);
+	matrix = create_matrix(nodes, edges, num_nodes);
+	find_paths(matrix, nodes, num_nodes);
 	fclose(f);
 }
 
-void	create_matrix(t_node **nodes, t_edge **edges)
+void	find_paths(int **matrix, t_node **nodes, int num_nodes)
+{
+	int	i;
+	int	j;
+	t_node	**path;
+
+	path = malloc(sizeof(t_node *));
+	path[0] = 0;
+	j = 0;
+	i = 0;
+	path[0] = malloc(sizeof(path));
+	path[0] = 0;
+	add_to_path(path, "start");
+	while (i < num_nodes - 1)
+	{
+		if (matrix[i][0] == 1)
+		{
+			add_to_path(path, get_node_name(nodes, i));
+		//	printf("%i: %s\n", i, get_node_name(nodes, i));
+			continue_path(matrix, nodes, path, num_nodes, i, &j);
+		}
+		i++;
+	}
+}
+
+void	continue_path(int **matrix, t_node **nodes, t_node **path, int num_nodes, int next_step, int *j)
+{
+	int	i;
+
+	i = 1;
+	if (next_step == num_nodes - 1)
+	{
+		print_path(path);
+		return ;
+	}
+	(*j)++;
+	while (i < num_nodes)// && *j < 20)
+	{
+		if (matrix[next_step][i] == 1)
+		{
+
+			printf("%i %i: %s\n", next_step, i, get_node_name(nodes, i));
+			if (check_path(path, next_step, get_node_name(nodes, i)))
+			{
+				add_to_path(path, get_node_name(nodes, i));
+				continue_path(matrix, nodes, path, num_nodes, i, j);
+			}
+		}
+		i++;
+	}
+}
+
+int	check_path(t_node **path, int current, char	*next)
+{
+	t_node	*prev_prev;
+//	t_node	*prev;
+	t_node	*aux;
+	int		i;
+
+	i = 0;
+	prev_prev = *path;
+	aux = *path;
+	while (aux)
+	{
+		if (next[0] >= 'a' && strcmp(next, aux->name) == 0)
+			return (0);
+		aux = aux->next;
+	}
+	current += 0;
+	return (1);
+/*	while (i < get_num_nodes(path) - 3)
+	{
+		prev_prev = prev_prev->next;
+		i++;
+	}
+	prev = prev_prev->next;
+	aux = prev->next;
+	printf("%i: %s, %s. %s, %s\n", current, prev_prev->name, aux->name, prev->name, next);
+	if (strcmp(prev_prev->name, aux->name) == 0 && strcmp(prev->name, next) == 0)
+	{
+		printf("coinc\n");
+		return (0);
+	}*/
+	return (1);
+}
+
+void	print_path(t_node **path)
+{
+	t_node	*aux;
+	aux = *path;
+	printf("path: ");
+	while (aux)
+	{
+		printf("%s,", aux->name);
+		aux = aux->next;
+	}
+	printf("\n");
+}
+
+
+void	add_to_path(t_node **path, char *name)
+{
+	t_node	*aux;
+	t_node	*new;
+
+	aux = *path;
+	new = malloc(sizeof(t_node));
+	new->name = strdup(name);
+	new->next = 0;
+	if (!aux)
+	{
+		*path = new;
+		return ;
+	}
+	while (aux->next)
+		aux = aux->next;
+	aux->next = new;
+}
+
+int	get_num_nodes(t_node **nodes)
+{
+	t_node	*aux_nodes;
+	int		n_nodes;
+
+	n_nodes = 0;
+	aux_nodes = *nodes;
+	while (aux_nodes)
+	{
+		aux_nodes = aux_nodes->next;
+		n_nodes++;
+	}
+	return (n_nodes);
+}
+
+int	**create_matrix(t_node **nodes, t_edge **edges, int	n_nodes)
 {
 	int		**matrix;
-	int		n_nodes;
-	t_node	*aux_nodes;
 	int		i;
 	int		j;
 	t_edge	*edge_aux;
@@ -61,15 +204,8 @@ void	create_matrix(t_node **nodes, t_edge **edges)
 	node_name = 0;
 	i = 0;
 	j = 0;
-	n_nodes = 0;
 	matrix = 0;
-	aux_nodes = *nodes;
 	edge_aux = *edges;
-	while (aux_nodes)
-	{
-		aux_nodes = aux_nodes->next;
-		n_nodes++;
-	}
 	matrix = malloc(sizeof(int *) * n_nodes);
 	while (i < n_nodes)
 	{
@@ -102,7 +238,6 @@ void	create_matrix(t_node **nodes, t_edge **edges)
 		}
 		i++;
 	}
-
 	i = 0;
 	while (i < n_nodes)
 	{
@@ -116,6 +251,7 @@ void	create_matrix(t_node **nodes, t_edge **edges)
 		printf("\n");
 		i++;
 	}
+	return (matrix);
 }
 
 int	get_node_position(t_node **nodes, char *name)
