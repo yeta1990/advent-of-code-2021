c1fb35297be2a10b81e0aa8501a900bb5bd16106
albgarci <albgarci@student.42madrid.com>
2021-12-29 23:25:21 +0100

    parse working

--- day-18/snailfish.c
+++ day-18/snailfish.c
@@ -19,7 +19,7 @@ typedef struct s_pair
 }	t_pair;
 
 t_pair	*new_pair(void);
-t_pair	*parse_snailfish_num(char *str);
+t_pair	*parse_snailfish_num(char **str);
 void	print_pair(t_pair *p);
 t_pair	*sum_snailfish_num(t_pair *p1, t_pair *p2);
 
@@ -30,14 +30,15 @@ int	main(void)
 //	t_pair	*sum;
 
 //	char	*str = "[1,2]";
-//	char	*str2 = "[[1,2],3]";
+//	char	*str = "[[1,2],3]";
 //	char	*str = "[2,[8,8]]";
-//	char	*str2 = "[8,[7,[1,0]]]";
-//	char	*str = "[[[[1,3],[5,3]],[[1,3],[8,7]]],[[[4,9],[6,9]],[[8,2],[7,3]]]]";
-	char	*str = "[[[1,3],[5,3]],[[1,3],[8,7]]]";
+//	char	*str = "[8,[7,[1,0]]]";
+	char	*str = "[[[[1,3],[5,3]],[[1,3],[8,7]]],[[[4,9],[6,9]],[[8,2],[7,3]]]]";
+//	char	*str = "[[[1,3],[5,3]],1]";
+//	char	*str = "[[1,3],[5,3]]";
 //	char	*str = "[7,[8,[7,[1,0]]]]";
 //	char	*str = "[[8,[7,[1,0]]],7]";
-	p = parse_snailfish_num(str);
+	p = parse_snailfish_num(&str);
 //	p2 = parse_snailfish_num(str2);
 	print_pair(p);
 //	sum = sum_snailfish_num(p, p2);
@@ -69,55 +70,47 @@ void	print_pair(t_pair *p)
 //	printf("\n");
 }
 
-t_pair *parse_snailfish_num(char *str)
+t_pair *parse_snailfish_num(char **str)
 {
 	t_pair	*p;
 
 	p = 0;
-	printf("->%s\n", str);
-	if (str && *str == '[')
+//	printf("->%s\n", *str);
+	if (*str && **str == '[')
 	{
 		p = new_pair();
-		str++;
-		if (isdigit((*str)))
+		(*str)++;
+		if (isdigit((**str)))
 		{
 			p->left->type = 0;
-			p->left->num = atoi(str);
-			while (isdigit((*str)))
-				str++;
+			p->left->num = atoi(*str);
+			while (isdigit((**str)))
+				(*str)++;
 		}
 		else
 		{
 			p->left->type = 1;
 			p->left->p = parse_snailfish_num(str);
-		//	p->left->p->parent = p;
-			printf("%li\n", strchr(str, ']') - str + 1);
-			str += strchr(str, ']') - str + 1;
-			printf("-->%s\n", str);
+			p->left->p->parent = p;
+			*str += strchr(*str, ']') - *str + 1;
 		}
-		if ((*str) == ',')
-			str++;
-		if (str && isdigit((*str)))
-		{
+		if ((**str) == ',')
+			(*str)++;
+		if (*str && isdigit((**str)))
+		{	
 			p->right->type = 0;
-			p->right->num = atoi(str);
-			while (isdigit((*str)))
-				str++;
+			p->right->num = atoi(*str);
+			while (isdigit((**str)))
+				(*str)++;
 		}
 		else
 		{
 			p->right->type = 1;
 			p->right->p = parse_snailfish_num(str);
-		//	p->right->p->parent = p;
-			printf("%li\n", strchr(str, ']') - str + 1);
-			str += strchr(str, ']') - str + 1;
-			printf("--->%s\n", str);
+			p->right->p->parent = p;
+			*str += strchr(*str, ']') - *str + 1;
 		}
-		str++;
 	}
-	printf("<-%s\n", str);
-	//	str++;
-//	}
 	return (p);
 }
 
